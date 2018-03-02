76605bf034839135f60c0ea41ea86e479c9aef53
Calebe Emerick <cemerik@stone.com.br>
2018-03-02 15:25:03 -0300

    Cria o modelo que representa um fato do Chuck Norris.

---
+++ Source/ChuckFacts/Fact/FactCategory.swift
@@ -0,0 +1,14 @@
+//
+//  FactCategory.swift
+//  ChuckFacts
+//
+//  Created by Calebe Emerick on 02/03/2018.
+//  Copyright © 2018 Stone Pagamentos. All rights reserved.
+//
+
+import Foundation
+
+struct FactCategory {
+	
+	let categories: [String]
+}
